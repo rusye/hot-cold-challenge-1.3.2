43a9d8d6bd41f18aad090e6b0042233886231a6c
Russ yev <ruslanyev@gmail.com>
2019-01-08 12:16:28 -0800

    deploying to heroku

--- src/components/game.test.js
+++ src/components/game.test.js
@@ -12,21 +12,71 @@ describe('<Game />', () => {
     shallow(<Game />);
   });
 
-  // it('Renders the guess button initially', () => {
-  //   const wrapper = shallow(<Game />);
-  //   expect(wrapper.hasClass('Header')).toEqual(true);
-  // });
-
   it('Imports all the classes properly', () => {
     const wrapper = shallow(<Game />);
     expect(wrapper.find(Header, GuessSection, StatusSection, InfoSection));
   });
 
   it('Should make sure that guesses array is empty', () => {
-    const wrapper = shallow(<Game />);
-    // console.log(wrapper.instance().state);
-    // console.log(wrapper);
-    // expect(wrapper.state().guesses).to.equal(0); 
+    const wrapper = shallow(<Game />); 
     expect(wrapper.instance().state.guesses.length).toEqual(0);   
+  });
+
+  it('Can start a new game', () => {
+    const wrapper = shallow(<Game />);
+    wrapper.setState({
+      guesses: [1, 2, 3, 4],
+      feedback: 'Awesome',
+      correctAnswer: -1
+    });
+    wrapper.instance().restartGame();
+    expect(wrapper.state('guesses')).toEqual([]);
+    expect(wrapper.state('feedback')).toEqual('Make your guess!');
+    expect(wrapper.state('correctAnswer')).toBeGreaterThanOrEqual(0);
+    expect(wrapper.state('correctAnswer')).toBeLessThanOrEqual(100);
+  });
+
+  it('Can make guesses', () => {
+    const wrapper = shallow(<Game />);
+    // Set the correctAnswer so we know what we're aiming for
+    wrapper.setState({
+      correctAnswer: 100
+    });
+
+    wrapper.instance().makeGuess(25);
+    expect(wrapper.state('guesses')).toEqual([25]);
+    expect(wrapper.state('feedback')).toEqual('You\'re Ice Cold...');
+
+    wrapper.instance().makeGuess(60);
+    expect(wrapper.state('guesses')).toEqual([25, 60]);
+    expect(wrapper.state('feedback')).toEqual('You\'re Cold...');
+
+    wrapper.instance().makeGuess(80);
+    expect(wrapper.state('guesses')).toEqual([25, 60, 80]);
+    expect(wrapper.state('feedback')).toEqual('You\'re Warm.');
+
+    wrapper.instance().makeGuess(95);
+    expect(wrapper.state('guesses')).toEqual([25, 60, 80, 95]);
+    expect(wrapper.state('feedback')).toEqual('You\'re Hot!');
+
+    wrapper.instance().makeGuess(100);
+    expect(wrapper.state('guesses')).toEqual([25, 60, 80, 95, 100]);
+    expect(wrapper.state('feedback')).toEqual('You got it!');
+  });
+
+  it('Can generate aural updates', () => {
+    const wrapper = shallow(<Game />);
+
+    wrapper.setState({
+      correctAnswer: 100
+    });
+
+    wrapper.instance().makeGuess(25);
+    wrapper.instance().makeGuess(3);
+    wrapper.instance().makeGuess(90);
+
+    wrapper.instance().generateAuralUpdate();
+
+    expect(wrapper.state('auralStatus')).toEqual('Here\'s the status of the game right now: You\'re Warm. You\'ve made 3 guesses. In order of most- to least-recent, they are: 90, 3, 25');
   });
 })
